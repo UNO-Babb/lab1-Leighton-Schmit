ec3a61bfae7c9d90400c6252401571463e3cfd21
Leighton-Schmit <lschmit@unomaha.edu>
2024-08-28 16:45:13 +0000

    first update

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,14 +6,20 @@
 def main():
   print("First Program")
   #Say hello
-  
+  print ("Hello")
   #Ask for the user's name
-
+  print ("What is your name?")
   #Use the user's name in the program.
-
+  name = input("What is your name?: ")
+  print(name)
   #Ask the user for their age.
-
+  print ("How old are you?")
+  age = input("How old are you?: ")
+  print(age)
   #Tell the user what year they were born in.
+  age = int(age)
+  born=(2024-(age))
+  print ("you were born in",born)
   #Assume that they have not had their birthday yet this year.
 
 
--- MadLib.py
+++ MadLib.py
@@ -6,11 +6,21 @@
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1 = input ("Enter a noun:")
+  name1 = input ("Enter a name!:")
+  noun2 = input("Enter second noun:")
+  adjective1 = input("Enter an adjective:")
+  verb1 = input ("Enter a verb:")
+  adjective2 = input ("Enter another adjective:")
+  name2 = input ("Enter another name:")
+  verb2 = input ("Enter another verb:")
   #Print the story with the user supplied words.
-
-
-
+  print ("Once upon a time, there was a knight, sir", (name2) )
+  print ("The knight was on the run because he" + (verb2) + (name1) + "in the face")
+  print ((name2) + "fled to the distasteful place of" + (noun1) + "and said it was quite" + (adjective2))
+  print ((name1) + "Followed the knight, and found out what the knights most precious item was, a " +(noun2))
+  print ("sir" + (name2) + "loved this eel because it was quite " + (adjective1))
+  print ("sir" + (name2) + "then " + (verb2) + (name1) + "and " + (verb1) + "his " +(noun2) + "and ran away again")
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
